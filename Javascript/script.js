2f1bf763b6089638e38f67e213e50cb9a01313ae
Iago Oliveira <Iagogoddigital@hotmail.com>
2023-12-28 18:52:59 -0300

    Construindo a parte principal e a das testemunhas

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,7 @@
-function toggleMenu(){
-  const nav_mobile = document.getElementsByClassName("links")[0];
-  nav_mobile.classList.toggle('links-show')  
 
-}
+// function toggleMenu(){
+//   const nav_mobile = document.getElementsByClassName("links")[0];
+//   nav_mobile.classList.toggle('links-show')  
+
+// }
+
